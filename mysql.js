697bfc3075d43a87451947702fa3935aa2a3e76c
fagner <100000573+fagnerdk@users.noreply.github.com>
2024-03-24 00:42:01 -0300

    ok

--- mysql.js
+++ mysql.js
@@ -1,17 +1,24 @@
 import mysql from 'mysql';
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  port:3306,
-  database: "db_açoes",
+
+  host: "dpg-cn2m5kmd3nmc739esg40-a",
+  user: "dbacoes_user",
+  password: "kYBubTiYbIGeBZY4OvevbamLi4RpjSDM",
+  port:5432,
+  database: "dbacoes",
   
 });
+//postgres://dbacoes_user:kYBubTiYbIGeBZY4OvevbamLi4RpjSDM@dpg-cn2m5kmd3nmc739esg40-a/dbacoes
+con.connect(function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected!");
 
+});
 
-
-export {con}
+//export {con}
 
 
 
